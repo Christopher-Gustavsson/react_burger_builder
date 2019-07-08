0358c71a74c8b46a0e1685801ce824a4326d6c37
Christopher Gustavsson <chrisgustavsson1@gmail.com>
2019-07-08 22:18:17 +0000

    the burger is now being rendered using ingredients from state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,1 +1,24 @@
 import React from 'react';
+import BurgerIngredient from './BurgerIngredient/BurgerInredient';
+import './Burger.css';
+
+const burger = props => {
+    
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(ingredKey => { 
+            return [...Array(props.ingredients[ingredKey])]
+                .map((_, index) => {
+                    return <BurgerIngredient key={ingredKey + index} type={ingredKey} />;
+                });
+        });
+
+    return(
+        <div className='Burger'>
+            <BurgerIngredient type='bread-top' />
+            {transformedIngredients}
+            <BurgerIngredient type='bread-bottom' />
+        </div>
+    );
+};
+
+export default burger;
